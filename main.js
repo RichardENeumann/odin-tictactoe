4c449ea5a96dfbf8dea267acda212d374e598596
RichardENeumann <RichardNeumann@googlemail.com>
2023-11-27 13:28:13 +0100

    Implement player object

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+function createPlayer(name, sign) {
+    let Player = (() => {
+        const _name = name;
+        const _sign = sign;
+
+        const getName = () => {
+            return _name;
+        }
+        return {
+            getName,
+        }
+    })();
+    return Player;
+};
+
+const playerA = createPlayer("Richard", "X");
+const playerB = createPlayer("Geena", "O");
